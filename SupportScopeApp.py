b832710caaf9fd5eb9663366d8ace15a286d6919
Kevin Malachin <kevinmalachin@gmail.com>
2024-06-13 12:57:39 +0200

    App creation

--- SupportScopeApp.py
+++ SupportScopeApp.py
@@ -1,21 +1,25 @@
 from bs4 import BeautifulSoup
 
-# Inserisci il percorso del tuo file HTML locale
-file_path = '/Users/kevin.malachin/Documents/SupportScopeSheets&HTML/Bouygues.html'
+def main():
+    # Inserisci il percorso del tuo file HTML locale
+    file_path = '/Users/kevin.malachin/Documents/SupportScopeSheets&HTML/Bouygues.html'
 
-# Leggi il contenuto del file HTML
-with open(file_path, 'r', encoding='utf-8') as file:
-    website_content = file.read()
+    # Leggi il contenuto del file HTML
+    with open(file_path, 'r', encoding='utf-8') as file:
+        website_content = file.read()
 
-# Crea l'oggetto BeautifulSoup
-soup = BeautifulSoup(website_content, 'html.parser')
+    # Crea l'oggetto BeautifulSoup
+    soup = BeautifulSoup(website_content, 'html.parser')
 
-# Trova tutti i tag <a> con le classi specificate
-classToFind = soup.find_all('a', class_='sc-csuQGl fgtqry')
+    # Trova tutti i tag <a> con le classi specificate
+    classToFind = soup.find_all('a', class_='sc-csuQGl fgtqry')
 
-# Stampa il contenuto HTML in un formato leggibile (opzionale)
-print(soup.prettify())
+    # Stampa il contenuto HTML in un formato leggibile (opzionale)
+    print(soup.prettify())
 
-# Estrai e stampa i testi di ogni tag trovato
-for tag in classToFind:
-    print(tag.get_text())
+    # Estrai e stampa i testi di ogni tag trovato
+    for tag in classToFind:
+        print(tag.get_text())
+
+if __name__ == "__main__":
+    main()

d73dcf981ed7876d5f16b12fd4b4e6d5bdae35d1
Kevin Malachin <kevinmalachin@gmail.com>
2024-06-13 20:38:47 +0200

    Check column in Excel for different group

--- SupportScopeApp.py
+++ SupportScopeApp.py
@@ -1,11 +1,15 @@
+import pandas as pd
 from bs4 import BeautifulSoup
 
 def main():
-    # Inserisci il percorso del tuo file HTML locale
-    file_path = '/Users/kevin.malachin/Documents/SupportScopeSheets&HTML/Bouygues.html'
+    # Percorso del file HTML locale
+    html_file_path = r'C:\Users\kevin\Documents\Automatizzazioni\Bouygues.html'
+    
+    # Percorso del file Excel locale
+    excel_file_path = r'C:\Users\kevin\Documents\Automatizzazioni\Bouygues.xlsx'
 
     # Leggi il contenuto del file HTML
-    with open(file_path, 'r', encoding='utf-8') as file:
+    with open(html_file_path, 'r', encoding='utf-8') as file:
         website_content = file.read()
 
     # Crea l'oggetto BeautifulSoup
@@ -14,12 +18,28 @@ def main():
     # Trova tutti i tag <a> con le classi specificate
     classToFind = soup.find_all('a', class_='sc-csuQGl fgtqry')
 
-    # Stampa il contenuto HTML in un formato leggibile (opzionale)
-    print(soup.prettify())
+    # Estrai i testi di ogni tag trovato
+    html_texts = {tag.get_text().strip() for tag in classToFind}  # Utilizzo di un set per i testi HTML
 
-    # Estrai e stampa i testi di ogni tag trovato
-    for tag in classToFind:
-        print(tag.get_text())
+    # Leggi il file Excel
+    df = pd.read_excel(excel_file_path, sheet_name='APIs Scope')
+
+    # Chiedi all'utente di inserire il nome del progetto da cercare nella colonna "Project"
+    project_name = input("Inserisci il nome del progetto (es. KAM): ").strip()
+
+    # Filtra il DataFrame per il valore specificato nella colonna "Project"
+    filtered_df = df[df['Project'] == project_name]
+
+    # Estrai i valori della colonna "APIs Name" dal DataFrame filtrato
+    excel_values = {name.strip() for name in filtered_df['APIs Name'].tolist()}  # Utilizzo di un set per i valori Excel
+
+    # Trova i nomi che sono presenti solo nell'HTML o solo nel file Excel filtrato
+    unique_names = html_texts.difference(excel_values)
+
+    # Stampa i nomi unici trovati
+    print(f"Nomi unici trovati nel progetto '{project_name}':")
+    for name in unique_names:
+        print(name)
 
 if __name__ == "__main__":
     main()
